a57bcbacd71e116daa03fd013c457992f0d8b2e5
Bluee <47085292+Bluee21@users.noreply.github.com>
2019-01-30 19:10:52 -0400

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -9,11 +9,20 @@ string s0;
 string s1 = "hello";
 string s2 = s1+"World";
 
-
+//printing
 cout << "s0 = " << s0<< endl;
 cout << "s1 = " << s1<< endl;
 cout << " the 4th character of S1 is " << s1 [3] << endl;
 cout << "s1 has" << s1.size() << "characters"<< endl; 
+
+  //comparison
+if ( s1 == "hello")
+  cout << "hi there" << endl;
+  if (s2 != "World")
+    cout << "out of this world" << endl;
+  
+  s1 += "Kitty";
+  
 return 0;
 
 }

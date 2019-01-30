e28de75e0577a66a1067bb08be7f01e91eb2d3eb
Bluee21 <Bluee21@users.noreply.github.com>
2019-01-30 11:46:18 -0400

    Added source file

---
+++ first.cpp
@@ -0,0 +1,19 @@
+using namespace std
+#include <string>
+#include <iostream>
+
+int main()
+{
+
+string s0;
+string s1 = "hello";
+string s2 = s1+"World";
+
+
+cout << "s0 = " << s0<< endl;
+cout << "s1 = " << s1<< endl;
+cout << " the 4th character of S1 is " << s1 [3] << endl;
+cout << "s1 has" << s1.size() << "characters"<< endl; 
+return 0;
+
+}
